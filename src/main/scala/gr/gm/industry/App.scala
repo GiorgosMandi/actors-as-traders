3a94f27c2c9b16654d3830687dbb5ef3fc5308c3
George Mandilaras <gmandilaras@ubitech.eu>
2022-11-13 14:18:35 +0200

    app

---
+++ src/main/scala/gr/gm/industry/App.scala
@@ -0,0 +1,23 @@
+package gr.gm.industry
+
+import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.stream.scaladsl.Sink
+import com.typesafe.config.{Config, ConfigFactory}
+import gr.gm.industry.graphs.PriceFlow
+import gr.gm.industry.graphs.actors.SimpleTrader
+import gr.gm.industry.listeners.BinanceListener
+
+object App extends App {
+
+    val conf: Config = ConfigFactory.load()
+    implicit val system: ActorSystem = ActorSystem("CryptoTrader", conf)
+    val priceSource = BinanceListener(parallelism = 5, throttle = (2, 1))
+    val priceFlow = PriceFlow.priceFlow
+    val trader: ActorRef = system.actorOf(Props[SimpleTrader], "SimpleTrader")
+
+
+    priceSource.apply
+      .via(priceFlow)
+      .to(Sink.foreach { case (tradeAction, priceDao) => trader ! (tradeAction, priceDao.price) })
+      .run()
+}
